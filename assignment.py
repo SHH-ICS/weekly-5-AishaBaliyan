ae4580dc532159d5e283240d5acddce26c3f7281
Aisha <115123499+AishaBaliyan@users.noreply.github.com>
2024-11-28 20:44:47 -0500

    had to change input variable name in order to do error checking

--- assignment.py
+++ assignment.py
@@ -1,7 +1,14 @@
-input = int(input("enter the number: "))
-j = 1    
-i = 0  
-for n in range(input + 1): 
-    i += j * (4 / (2 * n + 1))
-    j *= -1                
-print(i)
+number = (input("enter the number: "))
+if number.isdigit():  
+    number = int(number)
+    if number < 0:
+            print("error")
+    else:
+        j = 1    
+        i = 0  
+        for n in range(number + 1): 
+            i += j * (4 / (2 * n + 1))
+            j *= -1                
+        print(i)
+else:
+    print("error")
