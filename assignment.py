1e5f5455315d0043b1043e9220d118061b192a6b
Aisha <115123499+AishaBaliyan@users.noreply.github.com>
2024-11-28 20:05:26 -0500

    3+ version (no rounding will ask Julia how to)

--- assignment.py
+++ assignment.py
@@ -1,6 +1,7 @@
-def calculatePi(digits):
-  result = 0
-  return result
-
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+input = int(input("enter the number: "))
+j = 1    
+i = 0  
+for n in range(input + 1): 
+    i += j * (4 / (2 * n + 1))
+    j *= -1                
+print(i)
